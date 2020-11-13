d1b9b4b2409a85c683303aedd53cc6686ca72756
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-13 13:33:45 +0100

    Modification de syntaxe de fonction

--- exercice-02/assets/js/script.js
+++ exercice-02/assets/js/script.js
@@ -1,24 +1,20 @@
 let boldButton = document.getElementById('buttonBold');
-let ColorButton = document.getElementById('buttonColor');
+let colorButton = document.getElementById('buttonColor');
 let zoomButton = document.getElementById('buttonZoom');
 let divMain = document.getElementById('divMain');
 
-function getBold(element) {
-    element.classList.toggle('font-weight-bold');
+getBold = () => {
+    divMain.classList.toggle('font-weight-bold');
 };
 
-function getColor(element) {
-    element.classList.toggle('text-danger');
+getColor = () => {
+    divMain.classList.toggle('text-danger');
 };
 
-function getZoom(element) {
-    element.classList.toggle('zoom');
+getZoom = () => {
+    divMain.classList.toggle('zoom');
 };
 
-boldButton.addEventListener('click', getBold(divMain));
-ColorButton.addEventListener('click', getColor(divMain));
-zoomButton.addEventListener('click', getZoom(divMain));
-
-// onclick="getBold(document.getElementById('divMain'))"
-// onclick="getColor(document.getElementById('divMain'))"
-// onclick="getZoom(document.getElementById('divMain'))"
+boldButton.addEventListener('click', getBold);
+colorButton.addEventListener('click', getColor);
+zoomButton.addEventListener('click', getZoom);
